2826a21b8d13df9a31a20d6e4d169571094cc216
Jose Fernandez <jmfernandezg.awe@gmail.com>
2023-05-13 19:26:11 -0600

    Setting up Jetpack Compose

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,16 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
 }
 
 rootProject.name = "JetPackCompose"
-include("app")
+
